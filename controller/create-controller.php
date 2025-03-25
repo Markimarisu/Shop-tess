9a1d13a0af44f04847b7e5d690600cbed8632905
Markimarisu <marius.iurascu@lapiscine.pro>
2025-03-25 16:14:03 +0100

    shop-tess

--- controller/create-controller.php
+++ controller/create-controller.php
@@ -1,2 +1,17 @@
 <?php
-require_once('../view/create-view.php');
+
+require_once('../config.php');
+
+$message = "";
+
+if (array_key_exists("quantity", $_POST) && 
+	array_key_exists("product", $_POST))
+{
+	$product = $_POST["product"];
+	$quantity = $_POST["quantity"];
+
+	$message = "Votre commande contient : " .  $quantity . " " . $product;
+}
+
+
+require_once('../view/create-order.view.php');

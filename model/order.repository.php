eaf71a346d81ddb561c5ba8ecb63f164e3b755e0
Markimarisu <marius.iurascu@lapiscine.pro>
2025-04-03 16:29:52 +0200

    shop-tess

--- model/order.repository.php
+++ model/order.repository.php
@@ -1,6 +1,7 @@
 <?php
 
 
+//SELECT * FROM order where user.id = $id
 function findOrderByUser() {
 	if (array_key_exists("order", $_SESSION)) {
 		return $_SESSION["order"];
@@ -9,16 +10,28 @@ function findOrderByUser() {
 	}
 }
 
+// vérifie que la commande a le droit d'être créée
+// créé la commande
 function createOrder($product, $quantity) {
-	$order = [
-		"product" => $product,
-		"quantity" => $quantity
-	];
 
-	return $order;
+	if ($quantity < 0) {
+		throw new Exception("Interdiction de mettre une quantité inférieure à 0");
+	} else if ($quantity > 3){
+		throw new Exception("Interdication de mettre quantité supérieur à 3");
+		
+	} else {
+		$order = [
+			"product" => $product,
+			"quantity" => $quantity,
+			"createdAt" => new DateTime()
+		];
+	
+		return $order;
+	}
 }
 
 
+// INSERT INTO order values ($order['product'], $order['quantity'])
 function saveOrder($order) {
 	$_SESSION["order"] = $order;
 }

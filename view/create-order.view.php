1773a67c46ae642cb691a1944784a195ed212800
Markimarisu <marius.iurascu@lapiscine.pro>
2025-04-04 14:43:18 +0200

    shop-tess

--- view/create-order.view.php
+++ view/create-order.view.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-<body>
+<?php require('../view/partial/_header.view.php'); ?>
 
-	<header>
-
-		<nav>
-			<ul>
-				<li>Créer une commande</li>
-			</ul>
-		</nav>
-
-	</header>
-
+<?php require('../view/partial/_header.view.php'); ?>
 
 	<main>
 

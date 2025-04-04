4927f2ad74e3e9a9a8249def123b0de5a662949f
Markimarisu <marius.iurascu@lapiscine.pro>
2025-04-04 16:26:56 +0200

    shop-tess

--- view/partial/_header.view.php
+++ view/partial/_header.view.php
@@ -1,5 +1,4 @@
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,6 +14,7 @@
 			<ul>
 				<li><a href="http://localhost:8888/piscine-shop-tees/controller/create-order.controller.php">Créer une commande</a></li>
 				<li><a href="http://localhost:8888/piscine-shop-tees/controller/pay-order.controller.php">Payer la commande</a></li>
+				<li><a href="http://localhost:8888/piscine-shop-tees/controller/cancel-order.controller.php">Annuler la commande</a></li>
 			</ul>
 		</nav>
 
